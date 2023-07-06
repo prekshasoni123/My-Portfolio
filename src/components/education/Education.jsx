bf2296c34478be1e93cfd61a74b695872c6ce407
Preksha Soni <divysoni2812@gmail.com>
2023-07-06 13:15:48 +0530

    new commit

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,36 +1,88 @@
 import React from 'react'
 import './education.css'
-import AVTR1 from '../../required/angels.jpg'
-import AVTR2 from '../../required/svnit surat.jpg'
-import AVTR3 from '../../required/vvm.jpg'
+import AVTR3 from '../../required/angels.jpg'
+import AVTR1 from '../../required/svnit surat.jpg'
+import AVTR2 from '../../required/vvm.jpg'
 import Nav from '../nav/Nav'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data=[
+  {
+    avatar: AVTR1,
+    name: 'Sardar Vallabhbhai National Institute of Technology, Surat',
+    review1: 'Bachelors of Technology in',
+    review2: '"Electronics and Communication Engineering"',
+    review3:'2020-2024',
+    review4:'"CGPA: 8.57/10"'  
+  },
+
+  {
+    avatar: AVTR2,
+    name: 'Smt. M M Mehta English Medium School, Palanpur',
+    review1: 'Higher Secondary Education',
+    review2: 'Science/Maths (GSHEB)',
+    review3:'2018-2020',
+    review4:'Percentage: 79%'
+  },
+
+  {
+    avatar: AVTR3,
+    name: 'Angels English Medium School, Deesa',
+    review1: 'Primary and Secondary Education',
+    review2: 'Gujarat State Education Board',
+    review3:'2018',
+    review4:'Percentage: 93%'
+  },
+]
 
 export default function Education() {
   return (
+
+    
+
     <section id='education'>
       <h5>My Educational Qualification</h5>
       <h2>EDUCATION</h2>
 
-      <div className='container educations_container'>
-        <article className='education'>
-          <div className='edu_avatar'>
-            <img src='{AVTR1}' alt='Avatar One'/>
-            {/* <h3 className='inst_name'>Sardar Vallabhbhai National Institute of Technology, Surat</h3>
-         <br></br>
-         <br></br>
-            <div className='inst_details'>
-        <p>Bachelors of Technology</p>
-        <p>in</p>
-          <p>"Electronics and Communication Engineering"</p>
-          <br></br>
-          <p>2020-2024</p>
-         <br></br>
-         <p>"CGPA: 8.57"</p>
-          <br></br>
-        </div> */}
-          </div>
-        </article>
-      </div>
+
+      <Swiper className='container educations_container'>
+      
+        {
+          data.map(({avatar,name,review1,review2,review3,review4},index)=>{
+            return(
+              <SwiperSlide key={index} className='education'>
+                <div className='edu_avatar'>
+                  <img src={avatar}/>
+                </div>
+
+                <h5 className='inst_name'>{name}</h5>
+                <br/>
+                <div className='inst_details'>
+                  <p>{review1}</p>
+                  <br/>
+                  <p>{review2}</p>
+                  <br/>
+                  <p>{review3}</p>
+                  <br/>
+                  <p>{review4}</p>
+                </div>
+              </SwiperSlide>
+            )
+          })
+        }
+        
+      </Swiper>
+      
     </section>
   )
 }
